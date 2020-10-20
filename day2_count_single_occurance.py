0ca47070d2df463fd93a65657d0bcecaf27a9e1e
Yohaan <yozaam@gmail.com>
2020-10-20 04:09:26 +0000

    count occurance of single value

--- day2_count_single_occurance.py
+++ day2_count_single_occurance.py
@@ -1,6 +1,6 @@
 # Required concepts: https://www.w3schools.com/python/python_intro.asp Everything till for loops
 
-print('Question: count occurance of 2 in a number')
+print('Question: count occurance of digit 2 in a number')
 
 number = '222222222223456'
 count = 0
@@ -10,10 +10,10 @@ for digit in number:
   if digit == '2':
     count += 1
 
-# single line
+# single line, slower since we make a copy
 count = len(number) - len(number.replace('2',''))
 
 print('Answer:')
 print('2 has ',count,'occurances')
 
-#If you are same as your reverse, you are a palindrome
+# If you are same as your reverse, you are a palindrome
